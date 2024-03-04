980e24e0759a3f0ccf5932259ca6d9876d23cf39
Jason Lequenne <jsnlequenne@gmail.com>
2024-03-04 15:01:30 +0100

    ajout put & delete

--- route/animals.js
+++ route/animals.js
@@ -2,10 +2,10 @@ import express from 'express';
 const animalAPI = express.Router();
 
 const animals = [
-    {id: 1, name: 'Elephant', genre: 'Mammifère'},
-    {id: 2, name: 'Lion', genre: 'Félin'},
-    {id: 3, name: 'Aigle', genre: 'Rapace'},
-    {id: 4, name: 'Fourmie', genre: 'Insecte'}
+    { id: 1, name: 'Elephant', genre: 'Mammifère' },
+    { id: 2, name: 'Lion', genre: 'Félin' },
+    { id: 3, name: 'Aigle', genre: 'Rapace' },
+    { id: 4, name: 'Fourmie', genre: 'Insecte' }
 ];
 
 animalAPI.get('/animals', (req, res) => {
@@ -31,5 +31,20 @@ animalAPI.get('/animals/:id', (req, res) => {
         res.status(404).json({ message: 'animal not found' });
     }
 });
+
+animalAPI.put('/api/animals/:id', (req, res) => {
+    let { id } = req.params
+    const animal = animals.find(e => e.id === parseInt(id))
+    animal.name = req.query.name
+    res.send(animal)
+})
+
+animalAPI.delete('/api/animals/:id', (req, res) => {
+    let { id } = req.params
+    const animal = animals.find(e => e.id === parseInt(id))
+    const index = animals.indexOf(animal)
+    animals.splice(index, 1)
+    res.send('My animal has been deleted')
+})
 
 export default animalAPI;
