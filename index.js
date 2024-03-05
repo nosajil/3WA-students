300a2aff42cb44c296f928c09292e144216a6b61
Jason Lequenne <jsnlequenne@gmail.com>
2024-03-05 11:05:31 +0100

    création des views

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express';
-import animalAPI from './route/animals.js';
-
+import animalRouter from './route/animals.js';
+import path from 'node:path'
+import bodyParser from 'body-parser'
 const app = express();
-const port = 3000;
+const port = 3002;
 
-app.use('/', animalAPI)
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended : true
+}))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(process.cwd(), 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.use('/', animalRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- route/animals.js
+++ route/animals.js
@@ -1,5 +1,5 @@
 import express from 'express';
-const animalAPI = express.Router();
+const animalRouter = express.Router();
 
 const animals = [
     { id: 1, name: 'Elephant', genre: 'Mammifère' },
@@ -8,38 +8,39 @@ const animals = [
     { id: 4, name: 'Fourmie', genre: 'Insecte' }
 ];
 
-animalAPI.get('/animals', (req, res) => {
-    res.json(animals);
+animalRouter.get('/animals', (req, res) => {
+    res.render('animals', {animals}); 
 });
 
-animalAPI.post('/animals', (req, res) => {
-    const newanimal = {
-        id: animals.length + 1,
-        name: req.query.name,
-        genre: req.query.genre
-    };
-    animals.push(newanimal);
-    res.json(newanimal);
-});
-
-animalAPI.get('/animals/:id', (req, res) => {
-    const animalId = parseInt(req.params.id);
-    const animal = animals.find(animal => animal.id === animalId);
-    if (animal) {
-        res.json(animal);
-    } else {
-        res.status(404).json({ message: 'animal not found' });
-    }
-});
-
-animalAPI.put('/api/animals/:id', (req, res) => {
-    let { id } = req.params
-    const animal = animals.find(e => e.id === parseInt(id))
-    animal.name = req.query.name
-    res.send(animal)
+animalRouter.get('/animals/:id', (req, res) => {
+    const animal = animals.find(animal => animal.id === parseInt(req.params.id))
+    res.json(animal)
+    console.log(req.params.id);
 })
 
-animalAPI.delete('/api/animals/:id', (req, res) => {
+animalRouter.get('/addanimal', (req, res) => {
+    res.render('addAnimal')
+})
+
+animalRouter.post('/animals', (req, res) => {
+    const newanimal = {
+        id: animals.length + 1,
+        name: req.body.name,
+        genre: req.body.genre
+    };
+    animals.push(newanimal);
+    res.render('animals', {animals})
+});
+
+animalRouter.put('/api/animals/:id', (req, res) => {
+    let { id } = req.params
+    const animal = animals.find(e => e.id === parseInt(id))
+    animal.name = req.body.name
+    animal.genre = req.body.genre
+    res.json(movie)
+})
+
+animalRouter.delete('/api/animals/:id', (req, res) => {
     let { id } = req.params
     const animal = animals.find(e => e.id === parseInt(id))
     const index = animals.indexOf(animal)
@@ -47,4 +48,4 @@ animalAPI.delete('/api/animals/:id', (req, res) => {
     res.send('My animal has been deleted')
 })
 
-export default animalAPI;
+export default animalRouter;
